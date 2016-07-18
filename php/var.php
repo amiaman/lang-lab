9836972dec97517bc560396b139675dbafc41a11
Aman Kumar Singh <amanami00@gmail.com>
2016-07-18 12:37:57 +0530

    Create var.php

---
+++ php/var.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+	$txt = "Hello world!";
+	$x = 5;
+	$y = 10.5;
+	$a = "Siddharth"
+
+	echo "<br>";
+	echo $x;
+	echo "<br>";
+	echo $y . "<br>";
+	$z =  $y + $x;
+	echo $z;
+	unset($z);
+	echo $z;
+	var_dump($x);
+	var_dump($a);
+?>
+
+</body>
+</html>
