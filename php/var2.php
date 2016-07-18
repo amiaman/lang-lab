f2dc39364c1a281318ec396bc1d155949c9f7a22
Aman Kumar Singh <amanami00@gmail.com>
2016-07-18 12:38:44 +0530

    Create var2.php

---
+++ php/var2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE htlm>
+<html>
+<body>
+<?php
+ 
+	$name="Rajeev";
+ 
+	$$name="Sanjeev";
+ 
+	echo $name."<br/>";
+ 
+	echo $$name."<br/>";
+ 
+	echo $Rajeev . "<br>";
+	
+		
+?>
+</body>
+</htlm>
